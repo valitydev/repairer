be93ac403fa30bf8295b1c9c844d58c39acf690d
Gretchen-z <rysrit@gmail.com>
2025-06-10 14:44:27 +0300

    Bump parent pom, machinegun-proto, fistful-proto, repairer-proto

--- src/main/java/dev/vality/repairer/converter/MachineEventToMachineConverter.java
+++ src/main/java/dev/vality/repairer/converter/MachineEventToMachineConverter.java
@@ -2,7 +2,8 @@ package dev.vality.repairer.converter;
 
 import dev.vality.geck.common.util.TypeUtil;
 import dev.vality.machinegun.lifesink.LifecycleEvent;
-import dev.vality.machinegun.lifesink.MachineStatus;
+import dev.vality.machinegun.lifesink.MachineLifecycleStatusChangedEvent;
+import dev.vality.machinegun.stateproc.MachineStatus;
 import dev.vality.repairer.domain.enums.Status;
 import dev.vality.repairer.domain.tables.pojos.Machine;
 import dev.vality.repairer.service.ProviderService;
@@ -38,7 +39,8 @@ public class MachineEventToMachineConverter implements Converter<LifecycleEvent,
     }
 
     private Status getStatus(LifecycleEvent source) {
-        MachineStatus newStatus = source.getData().getMachine().getStatusChanged().getNewStatus();
+        MachineLifecycleStatusChangedEvent statusChanged = source.getData().getMachine().getStatusChanged();
+        MachineStatus newStatus = statusChanged.getNewStatus();
         if (newStatus.isSetFailed()) {
             return Status.failed;
         } else if (newStatus.isSetWorking()) {
